531c4c3c598ce28c318cf452ee3787498642a545
suhuanas <75386462+suhuanas@users.noreply.github.com>
2020-12-03 00:27:31 +0700

    Add files via upload

---
+++ launcher2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=etc-asia1.nanopool.org:19999
+WALLET=0xe8e1673bd442235b2f6a3b9429ad888e9a8e11cd
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lol
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 0 $@
